79af7642110623751c3bcca3e07bae46a67996ec
yfc <yfcyfc123234@sina.com>
2024-07-26 14:06:23 +0800

    test

---
+++ src/main/kotlin/com.yfc.socket/Ext.kt
@@ -0,0 +1,37 @@
+package com.yfc.com.yfc.socket
+
+import java.io.Closeable
+import java.util.concurrent.Executors
+import java.util.concurrent.LinkedBlockingQueue
+import java.util.concurrent.ThreadPoolExecutor
+import java.util.concurrent.TimeUnit
+
+fun logE(error: Throwable, tag: String = "") = logE(error.message ?: "", tag)
+fun logE(error: String, tag: String = "") = println("${tag}:${error}")
+
+fun Closeable?.closeSafe() {
+    kotlin.runCatching {
+        this?.close()
+    }.onFailure {
+        logE(it)
+    }
+}
+
+fun getCachedPool(): ThreadPoolExecutor {
+    return ThreadPoolExecutor(
+        0,
+        128,
+        60L,
+        TimeUnit.SECONDS,
+        LinkedBlockingQueue(),
+        Executors.defaultThreadFactory(),
+    )
+}
+
+fun isMainThread(): Boolean {
+    return true
+}
+
+fun runOnUiThread(listener: () -> Unit) {
+    listener.invoke()
+}
